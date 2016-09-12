ce4d1f62ce3b5d0e0dd1d12127485a9ea757a7cf
Mark Jordan <mjordan@sfu.ca>
2016-09-12 15:08:31 +0000

    Changed which URL parameters get passed into hook_islandora_rest_response; added HTTP response code as parameter to hook_islandora_rest_response; updates islandora_rest.api.php.

--- islandora_rest.api.php
+++ islandora_rest.api.php
@@ -7,6 +7,8 @@
 /**
  * Notify modules of requests to Islandora REST endpoints other than 'solr'.
  *
+ * @param string $http_response_code
+ *   The HTTP response code generated for the response.
  * @param string $endpoint
  *   The REST endpoint.
  * @param string $request_method
@@ -15,10 +17,10 @@
  *   The PID of the Islanora object.
  * @param string $dsid
  *   The DSID.
- * @param array $path_parameters
- *   An array containing all URL parameters included in the request.
+ * @param array $request_parameters
+ *   An array containing all request parameters.
  */
-function hook_islandora_rest_response($endpoint, $request_method, $pid, $dsid, $path_parameters) {
+function hook_islandora_rest_response($http_response_code, $endpoint, $request_method, $pid, $dsid, $request_parameters) {
   watchdog('islandora_rest', 'Islandora REST %method request for object: %object',
      array('%method' => $request_method, '%object' => $pid));
 }
@@ -26,11 +28,13 @@ function hook_islandora_rest_response($endpoint, $request_method, $pid, $dsid, $
 /**
  * Notify modules of requests to Islandora REST 'solr' endpoint.
  *
+ * @param string $http_response_code
+ *   The HTTP response code generated for the response.
  * @param string $query
  *   The Solr query.
- * @param array $parameters
+ * @param array $solr_parameters
  *   An array containing all Solr query parameters included in the request.
  */
-function hook_islandora_rest_solr_response($query, $parameters) {
+function hook_islandora_rest_solr_response($http_response_code, $query, $solr_parameters) {
   watchdog('islandora_rest', 'Islandora REST Solr query: %query', array('%query' => $query));
 }
